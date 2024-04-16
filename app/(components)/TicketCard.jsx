902d4f57cafd6e7bf96c9b50eaa1abc04c3409fb
Ezequiel Menor <ezequielmenor@gmail.com>
2024-04-16 15:40:50 +0200

    Tickets

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,15 +1,32 @@
 import DeleteBlock from "./DeleteBlock"
 import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
-      <DeleteBlock />
-      <PriorityDisplay />
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Titulo del Ticket</h4>
+      <hr className="h-px  border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">
+        Esta es la descripción del ticket! Por favor haga este ticket
+      </p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">16/04/24 15:38</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   )
 }
