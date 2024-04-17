48ad332af7f74ce14455ebdf6422f8aaee9c065a
Ezequiel Menor <ezequielmenor@gmail.com>
2024-04-17 16:51:38 +0200

    Eliminar tickets

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,13 +3,30 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
+
 const TicketCard = ({ ticket }) => {
+  const formatTimestamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    };
+
+    const date = new Date(timestamp)
+    const formattedDate = date.toLocaleDateString("es-ES", options);
+
+    return formattedDate;
+  };
+
   return (
     <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
         <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
-          <DeleteBlock />
+          <DeleteBlock id={ticket._id} />
         </div>
       </div>
       <h4>{ticket.title}</h4>
@@ -20,7 +37,7 @@ const TicketCard = ({ ticket }) => {
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
           <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
