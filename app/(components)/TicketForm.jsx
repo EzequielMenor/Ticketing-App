1a269628a01ab32f2734d91220561ae30a0a6fd1
Ezequiel Menor <ezequielmenor@gmail.com>
2024-04-17 10:30:04 +0200

    Crear Ticket

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -4,6 +4,34 @@ import { useRouter } from "next/navigation"
 import React, { useState } from "react";
 
 const TicketForm = () => {
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/Tickets", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      "Content-Type": "application/json",
+    })
+
+    if (!res.ok) {
+      throw new Error("Fallo al crear el Ticket.")
+    }
+
+    router.refresh()
+    router.push("/")
+  };
+
   const startingTicketData = {
     title: "",
     description: "",
@@ -16,8 +44,108 @@ const TicketForm = () => {
   const [formData, setFormData] = useState(startingTicketData);
   return (
     <div className="flex justify-center">
-      <form>
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <h3>Crea Tu Ticket</h3>
+        <label>Titulo</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+
+        <label>Descripción</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows="5"
+        />
+
+        <label>Categoría</label>
+        <select name="category" value={formData.category} onChange={handleChange}>
+          <option value="Problema de Hardware">Problema de Hardware</option>
+          <option value="Problema de Software">Problema de Software</option>
+          <option value="Proyecto">Proyecto</option>
+        </select>
+
+        <label>Prioridad</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+        <label>Progreso</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress}
+          min="0"
+          max="100"
+          onChange={handleChange}
+        />
+        <label>Estado</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="not started">No empezado</option>
+          <option value="started">Empezado</option>
+          <option value="done">Hecho</option>
+        </select>
+        <input type="submit" className="btn" value="Crear Ticket" />
       </form>
     </div>
   )
