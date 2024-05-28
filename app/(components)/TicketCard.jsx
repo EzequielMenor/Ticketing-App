2f8f26bb8676b6026328809133831514ad5b21af
Ezequiel Menor <ezequielmenor@gmail.com>
2024-05-28 09:45:57 +0200

    subir el proyecto

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,48 +1,45 @@
-import Link from "next/link";
-import DeleteBlock from "./DeleteBlock"
-import PriorityDisplay from "./PriorityDisplay";
-import ProgressDisplay from "./ProgressDisplay";
-import StatusDisplay from "./StatusDisplay";
-
+import Link from 'next/link'
+import DeleteBlock from './DeleteBlock'
+import PriorityDisplay from './PriorityDisplay'
+import ProgressDisplay from './ProgressDisplay'
+import StatusDisplay from './StatusDisplay'
 
 const TicketCard = ({ ticket }) => {
   const formatTimestamp = (timestamp) => {
     const options = {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
       hour12: false,
-    };
+    }
 
     const date = new Date(timestamp)
-    const formattedDate = date.toLocaleDateString("es-ES", options);
+    const formattedDate = date.toLocaleDateString('es-ES', options)
 
-    return formattedDate;
-  };
+    return formattedDate
+  }
 
   return (
-    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
-      <div className="flex mb-3">
+    <div className='flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2'>
+      <div className='flex mb-3'>
         <PriorityDisplay priority={ticket.priority} />
-        <div className="ml-auto">
+        <div className='ml-auto'>
           <DeleteBlock id={ticket._id} />
         </div>
       </div>
-      <Link href={`/TicketPage/${ticket._id}`} style={{ display: "contents" }}>
+      <Link href={`/TicketPage/${ticket._id}`} style={{ display: 'contents' }}>
         <h4>{ticket.title}</h4>
-        <hr className="h-px  border-0 bg-page mb-2" />
-        <p className="whitespace-pre-wrap">
-          {ticket.description}
-        </p>
-        <div className="flex-grow"></div>
-        <div className="flex mt-2">
-          <div className="flex flex-col">
-            <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
+        <hr className='h-px  border-0 bg-page mb-2' />
+        <p className='whitespace-pre-wrap'>{ticket.description}</p>
+        <div className='flex-grow'></div>
+        <div className='flex mt-2'>
+          <div className='flex flex-col'>
+            <p className='text-xs my-1'>{formatTimestamp(ticket.createdAt)}</p>
             <ProgressDisplay progress={ticket.progress} />
           </div>
-          <div className="ml-auto flex items-end">
+          <div className='ml-auto flex items-end'>
             <StatusDisplay status={ticket.status} />
           </div>
         </div>
@@ -51,4 +48,4 @@ const TicketCard = ({ ticket }) => {
   )
 }
 
-export default TicketCard;
+export default TicketCard
